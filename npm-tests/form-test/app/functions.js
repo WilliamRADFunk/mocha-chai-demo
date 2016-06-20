4b4f958897f39cbd187fbaa5b4f901d9b370e679
William Funk <wfunk@knights.ucf.edu>
2016-06-20 14:28:45 -0400

    Organized test cases to be better recognized when converting HTML output back into JSON for sending through Express and onward back to node for display in command line or anywhere else for that matter.

--- npm-tests/form-test/app/functions.js
+++ npm-tests/form-test/app/functions.js
@@ -7,39 +7,23 @@ function isEven(num)
   if (num%2 !== 0) return false;
   return true;
 }
+var rgbToHex = function(red, green, blue)
+{
+	var redHex   = red.toString(16);
+	var greenHex = green.toString(16);
+	var blueHex  = blue.toString(16);
 
-
-
-
-
-
-
-
-
-
-
-
-/*exports.rgbToHex = function(red, green, blue) {
-
-  var redHex   = red.toString(16);
-  var greenHex = green.toString(16);
-  var blueHex  = blue.toString(16);
-
-  return pad(redHex) + pad(greenHex) + pad(blueHex);
-
+	return pad(redHex) + pad(greenHex) + pad(blueHex);
 };
-
-function pad(hex) {
-  return (hex.length === 1 ? "0" + hex : hex);
+function pad(hex)
+{
+	return (hex.length === 1 ? "0" + hex : hex);
 }
+var hexToRgb = function(hex)
+{
+	var red   = parseInt(hex.substring(0, 2), 16);
+	var green = parseInt(hex.substring(2, 4), 16);
+	var blue  = parseInt(hex.substring(4, 6), 16);
 
-exports.hexToRgb = function(hex) {
-
-  var red   = parseInt(hex.substring(0, 2), 16);
-  var green = parseInt(hex.substring(2, 4), 16);
-  var blue  = parseInt(hex.substring(4, 6), 16);
-
-  return [red, green, blue];
-
+	return [red, green, blue];
 };
-*/
--- npm-tests/form-test/test/functions.js
+++ npm-tests/form-test/test/functions.js
@@ -1,18 +1,18 @@
 var expect = chai.expect;
 var should = chai.should();
 
-describe('Compare Numbers', function()
+describe('#Compare Numbers', function()
 {
 	it('1 should equal 1', function()
 	{
-		expect(1).to.equal(1);
+		expect(2).to.equal(1);
 	});
 	it('2 should be greater than 1', function()
 	{
 		expect(2).to.be.greaterThan(1);
 	});
 });
-describe('Is Even Tests', function()
+describe('#Is Even Tests', function()
 {
 	it('Should always return a boolean', function()
 	{
@@ -27,9 +27,39 @@ describe('Is Even Tests', function()
 		expect(isEven(77)).to.be.false;
 	});
 });
-describe("Input validation Functions", function()
+describe("@Color Code Converter", function()
 {
-	describe("First Name is a string without spaces or special character", function()
+	describe("#RGB to Hex conversion", function()
+	{
+		it("converts the basic colors", function()
+		{
+			var redHex   = rgbToHex(255, 0, 0);
+			var greenHex = rgbToHex(0, 255, 0);
+			var blueHex  = rgbToHex(0, 0, 255);
+
+			expect(redHex).to.equal("ff0000");
+			expect(greenHex).to.equal("00ff00");
+			expect(blueHex).to.equal("0000ff");
+		});
+	});
+
+	describe("#Hex to RGB conversion", function()
+	{
+		it("converts the basic colors", function()
+		{
+			var red   = hexToRgb("ff0000");
+			var green = hexToRgb("00ff00");
+			var blue  = hexToRgb("0000ff");
+
+			expect(red).to.deep.equal([255, 0, 0]);
+			expect(green).to.deep.equal([0, 255, 0]);
+			expect(blue).to.deep.equal([0, 0, 255]);
+		});
+	});
+});
+describe("@Input validation Functions", function()
+{
+	describe("#First Name is a string without spaces or special character", function()
 	{
 		it("First Name returns a string", function()
 		{
@@ -37,36 +67,3 @@ describe("Input validation Functions", function()
 		});
 	});
 });
-
-
-
-
-
-
-/*
-describe("Color Code Converter", function() {
-  describe("RGB to Hex conversion", function() {
-	it("converts the basic colors", function() {
-	  var redHex   = converter.rgbToHex(255, 0, 0);
-	  var greenHex = converter.rgbToHex(0, 255, 0);
-	  var blueHex  = converter.rgbToHex(0, 0, 255);
-
-	  expect(redHex).to.equal("ff0000");
-	  expect(greenHex).to.equal("00ff00");
-	  expect(blueHex).to.equal("0000ff");
-	});
-  });
-
-  describe("Hex to RGB conversion", function() {
-	it("converts the basic colors", function() {
-	  var red   = converter.hexToRgb("ff0000");
-	  var green = converter.hexToRgb("00ff00");
-	  var blue  = converter.hexToRgb("0000ff");
-
-	  expect(red).to.deep.equal([255, 0, 0]);
-	  expect(green).to.deep.equal([0, 255, 0]);
-	  expect(blue).to.deep.equal([0, 0, 255]);
-	});
-  });
-});
-*/

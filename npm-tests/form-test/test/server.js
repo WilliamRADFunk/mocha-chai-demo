17c99f02d9c2831fdacf87269a173aa01b4884f2
William Funk <wfunk@knights.ucf.edu>
2016-06-24 12:08:34 -0400

    Covered the double category scenario.

--- npm-tests/form-test/test/server.js
+++ npm-tests/form-test/test/server.js
@@ -39,7 +39,28 @@ function displayResults(results)
 		console.log("");
 		if(results[i].innerTests)
 		{
-
+			console.log("\t" + results[i].id.cyan.bold);
+			for(var j = 0; j < results[i].innerTests.length; j++)
+			{
+				for(var k = 0; k < results[i].innerTests[j].title.length; k++)
+				{
+					if(results[i].innerTests[j].status[k] == "passed")
+					{
+						var successOutput = '\u2713'.green + "\t"; // Green check mark
+						successOutput += results[i].innerTests[j].id + ": " + results[i].innerTests[j].title[k] + " "; // Category title and test title.
+						if(results[i].innerTests[j].speed[k] != "-1") successOutput += "(" + results[i].innerTests[j].speed[k] + ")"; // Speed output
+						console.log(successOutput);
+					}
+					else
+					{
+						var failureOutput = '\u2717'.red + "\t"; // Red X mark
+						failureOutput += results[i].innerTests[j].id + ": " + results[i].innerTests[j].title[k] + " "; // Category title and test title.
+						if(results[i].innerTests[j].speed[k] != "-1") failureOutput += "(" + results[i].innerTests[j].speed[k] + ")"; // Speed output
+						failureOutput += "\n\t\t" + results[i].innerTests[j].errorMsg[k].red; // Error message
+						console.log(failureOutput);
+					}
+				}
+			}
 		}
 		else
 		{
